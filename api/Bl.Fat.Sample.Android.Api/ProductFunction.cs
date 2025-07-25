8a735ae6128232cc68dc57ac861f6a9e35b5851e
Guilherme Bley <guilhermecbley@gmail.com>
2025-07-25 18:54:00 -0300

    Tela de Lista de Produtos por Categoria #3 - adding product api

---
+++ api/Bl.Fat.Sample.Android.Api/ProductFunction.cs
@@ -0,0 +1,47 @@
+﻿using Bl.Fat.Sample.Android.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Bl.Fat.Sample.Android.Api;
+
+public class ProductFunction
+{
+    private readonly ILogger<UserFunction> _logger;
+    private readonly MenuContext _menuContext;
+
+    public ProductFunction(
+        ILogger<UserFunction> logger,
+        MenuContext menuContext)
+    {
+        _logger = logger;
+        _menuContext = menuContext;
+    }
+
+    [Function("ProductFunctionGetAll")]
+    public async Task<IActionResult> GetProductsAsync(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "api/product")] HttpRequest req)
+    {
+        var items = await _menuContext
+            .Products
+            .AsNoTracking()
+            .Select(x => new
+            {
+                x.Id,
+                x.Name,
+                x.Description,
+                x.CreatedAt,
+                x.ImageUrl,
+                x.Price
+            }).ToListAsync();
+
+        if (items == null || !items.Any())
+        {
+            _logger.LogInformation("No products found.");
+            return new NotFoundResult();
+        }
+
+        return new OkObjectResult(items);
+    }
+}
